efb1998daa0f81216e57b787ffe569263e0d7a9a
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-06-12 09:49:03 +0530

    Update Day4Notes.js

--- Day4Notes.js
+++ Day4Notes.js
@@ -25,3 +25,19 @@ updated coode
 </script>
 </body>
 </html>
+
+
+
+put this Code into your file of body 
+
+<p>Say what? <input id="sayThis" size=40>
+<P>How many times? <select id='howMany'>
+<option value=1>1</option>
+<option value=5 selected>5</option>
+<option value=10>10</option>
+<option value=20>20</option>
+</select>
+  </p>
+<p><button onClick='doLoop()'>Do It!</button></p>
+<p><div id="results"></div></p>
+
