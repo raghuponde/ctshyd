2a14062252e84cbc5817db5366b023217acb77f5
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-06-12 10:07:52 +0530

    Update Day4Notes.js

--- Day4Notes.js
+++ Day4Notes.js
@@ -41,3 +41,43 @@ put this Code into your file of body
 <p><button onClick='doLoop()'>Do It!</button></p>
 <p><div id="results"></div></p>
 
+updated code 
+-------------
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Say what? <input id="sayThis" size=40>
+    <P>How many times? <select id='howMany'>
+            <option value=1>one</option>
+            <option value=5 selected>five</option>
+            <option value=10>ten</option>
+            <option value=20>twenty</option>
+        </select>
+    </p>
+    <p><button onClick='doLoop()'>Do It!</button></p>
+    <p>
+    <div id="results" style="background-color:yellow;color:red;height:200px;">
+        </div>
+    </p>
+    <script type="text/javascript">
+        function doLoop()
+        {
+        var saywhat = document.getElementById('sayThis').value;
+        var maxloop = document.getElementById('howMany').value;
+        var str = "";// to store the output temperorily
+        for (var i = 1; (i <= maxloop); i++) {
+
+            str = str + i + ':' + saywhat + '<br/>';
+
+        }
+        document.getElementById('results').innerHTML = str;
+    }
+    </script>
+</body>
+</html>
