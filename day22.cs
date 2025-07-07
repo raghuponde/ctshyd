e07a233ab382f91a4424be6d632e54b1385c554d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-07-08 01:13:53 +0530

    Create day22.cs

---
+++ day22.cs
@@ -0,0 +1,17 @@
+solid principles (week3 day 1) 
+------------------
+https://olympus1.mygreatlearning.com/online_session/recordings?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZW50b3JlZF9zZXNzaW9uX2lkIjoxMjYzMDMzfQ.IRdNwYRW-j6AE39Ygk1rRrcPi8R5seRIZzl9sndzcZg
+
+design patterns Week3 day 2
+---------------------------------
+https://olympus1.mygreatlearning.com/online_session/recordings?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZW50b3JlZF9zZXNzaW9uX2lkIjoxMjYzMDM1fQ.T9_xtraJfVHXP_jF7yuLyxJzErYU36rxJlyO7lwgW1A
+
+Great Learning:
+-----------------
+https://olympus.mygreatlearning.com/login
+
+Name:	Raghavendra Ponde
+Email:	raghuponde@yahoo.com
+password :Raghu#821519
+
+
