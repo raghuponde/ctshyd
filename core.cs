917114051d90fbafe9a28be7af4f0e4439aed7f6
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-07-14 00:40:43 +0530

    Update core.cs

--- core.cs
+++ core.cs
@@ -172,7 +172,165 @@ Now go to collectionofobjects.cs view and add there two links like this one is a
 
 I am telling here that in home controller there is details method so let us add taht method now 
 
+	public IActionResult Details(int id)
+	{
+		
 
+		var employee = emplist.FirstOrDefault(e => e.EmployeeID == id);
+		if (employee == null)
+		{
+			return NotFound();
+		}
+
+		return View(employee);
+	}
+
+and details view will be like tis 
+------------------------------------
+@model mvcwebapp1.Models.Employee
+@{
+    ViewData["Title"] = "Details";
+}
+
+<h1>Details</h1>
+
+<h1>@Model.EmpName</h1>
+
+<div>
+    <img src="@Model.ImageUrl" alt="@Model.EmpName" width="300" height="300" />
+</div>
+
+<div>
+  
+    <p><strong>Salary:</strong> @Model.Salary</p>
+</div>
+
+<a asp-action="Index">Back to List</a>
+
+Now let us try view model now 
+
+add one department class like this 
+
+namespace MvcFirstExample.Models
+{
+    public class Department
+    {
+
+        public int DeptId { set; get; }
+        public string? DeptName { set; get; }
+    }
+}
+
+
+
+ List<Department> deptlist = new List<Department>()
+     {
+         new Department{DeptId=10,DeptName="Sales"},
+         new Department{DeptId=20,DeptName="HR"},
+         new Department{DeptId=30,DeptName="Software"}
+     };
+
+
+public ActionResult viewmodeldemo(int empid)
+{
+	var query1 = deptlist.ToList();
+
+	Employee emp=emplist.Where(x=>x.EmployeeID==empid).FirstOrDefault();
+	var query2 = emp;
+
+	EmpDeptViewModel obj = new EmpDeptViewModel()
+	{
+		deptlist = query1,
+		emp = query2,
+		date = DateTime.Now
+	};
+
+	return View(obj);
+}
+
+Employeeviewmodel view 
+-------------------------
+@model MvcFirstExample.Models.EmpDeptViewModel
+@{
+    ViewData["Title"] = "viewmodeldemo";
+}
+
+<h1>viewmodeldemo</h1>
+
+<h2> Date : @Model.date</h2>
+
+<h3>dept details </h3>
+<table border="1" cellpadding="1" cellspacing="1">
+    <tr>
+        <th>DeptID</th>
+        <th>Deptname</th>
+    
+
+    @foreach (Department dept in Model.deptlist)
+    {
+            <tr>
+                <td>@dept.DeptId</td>
+                <td>@dept.DeptName</td>
+            </tr>
+    }
+
+
+</table>
+<h3> Search emp details</h3>
+<table border="1" cellpadding="0" cellspacing="0">
+        <tr>
+            <th>EmployeeID</th>
+            <th>Empname</th>
+            <th>EmpSalary</th>
+        </tr>
+        @if(Model.emp!=null)
+    {
+        <tr>
+            <td>@Model.emp.EmployeeID</td>
+            <td>@Model.emp.EmpName</td>
+            <td>@Model.emp.Salary</td>
+        </tr>
+    }
+    else
+    {
+        <tr>
+            <td>There is no employee with this id</td>
+        </tr>
+    }
+
+ </table>
+
+Search Employeee 
+------------------
+public ActionResult searchemp(int empid)
+{
+	Employee emp = (from e1 in emplist where e1.EmployeeID == empid select e1).FirstOrDefault();
+	return View(emp);
+}
+
+search emp view 
+------------------
+@model MvcFirstExample.Models.Employee
+@{
+    ViewData["Title"] = "searchemp";
+}
+
+<h1>searchemp</h1>
+
+  <table border="1" cellpadding="0" cellspacing="0">
+        <tr>
+            <th>EmployeeID</th>
+            <th>Empname</th>
+            <th>EmpSalary</th>
+        </tr>
+        <tr>
+            <td>@Model.EmployeeID</td>
+            <td>@Model.EmpName</td>
+            <td>@Model.Salary</td>
+        </tr>
+    </table>
+
+	
 CRUD operation using a Model :
 -----------------------------------------
 Means for a model means for a class any class i want to define insert,update ,delete and read etc methods in Controller function means function for insert and function for update and all i want to write 
@@ -344,6 +502,28 @@ namespace MvcFirstExample.Controllers
     }
 }
 
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcFirstExample.Models
+{
+    public class Dog
+    {
+
+        [Required(ErrorMessage = "ID is required")]
+        public int ID { set; get; }
+
+        [Required(ErrorMessage = "Name is required"), MaxLength(222)]
+        public string? Name { set; get; }
+
+        [Required(ErrorMessage = "Age is required"),
+            Range(0, 20, ErrorMessage = "Age should be between 0 to 20 only")]
+        public int Age { set; get; }
+
+
+
+
+    }
+}
 
 
 For designing first learn bootstrap from this link 
