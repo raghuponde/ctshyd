ebfa090b0a295e77b02f78a01a6413153c4bbd04
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-07-17 01:49:46 +0530

    Update core.cs

--- core.cs
+++ core.cs
@@ -1,335 +1,4 @@
 
-Exception :
-----------
-An abnormal event that disrupts the normal flow of programming is called as an exception 
-we can say run time error 
-
-
-eg: you are trying to open a file which is not present an exception will come 
-
-
-where there is a possiblity of error that code is kept in try block
-
-and for to catch error we use catch block where we write user defined message in catch .
-
-for one try block there can be multiple catch blocks based upon type of exceptions occur.
-
-whether exception comes or not comes i want my code to be executed i use finally block 
-
-
-try -->catch-->finally is okay 
-try -->catch is okay 
-try-->finally 
-
-in between try and catch nothing should be used except comments // 
-
-we can create our own exceptions and throw in .net there is no throws only throw is there in .net
-
-Base class of all exception is Exception class .
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
-namespace WindowsFormsApp7
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        public class ICICIBankException : ApplicationException
-        {
-            public ICICIBankException(string message) : base(message)
-            {
-
-            }
-        }
-       
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int a = Convert.ToInt32(textBox1.Text);
-                int b = Convert.ToInt32(textBox2.Text);
-                int c = a / b;
-                textBox3.Text = c.ToString();
-            }
-
-            //comments
-            catch (DivideByZeroException ee)
-            {
-                MessageBox.Show("dont enter second value  as zero..." + ee.Message);
-
-            }
-            catch (FormatException ee)
-            {
-                MessageBox.Show("dont enter charcters or special symbols.." + ee.Message);
-            }
-
-            catch (Exception ee)
-            {
-                MessageBox.Show("Some general errror came chk it.." + ee.Message);
-            }
-            finally
-            {
-                MessageBox.Show(" I am still alive..");
-            }
-        }
-
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-            try
-            {
-                int age = Convert.ToInt32(textBox4.Text);
-                if (age < 18)
-                {
-                    ICICIBankException obj = new ICICIBankException("ICICIBankException : Age above 18 to open accoutn");
-                    throw obj;
-                }
-                else
-                {
-                    MessageBox.Show("click this link to open an account :\"+\"https://www.icicibank.com/\"");
-                }
-            }
-            catch (ICICIBankException ice)
-            {
-                MessageBox.Show(ice.Message);
-            }
-        }
-    }
-}
-
-Now go to collectionofobjects.cs view and add there two links like this one is achcor link  and another is tag helper link as per latest trend okay 
-
-
-@model IEnumerable<mvcwebapp1.Models.Employee>
-@{
-    ViewData["Title"] = "collectionofobjectpassing";
-}
-
-<h1>collectionofobjectpassing</h1>
-
-<style>
-	table {
-		border-collapse: collapse;
-		width: 50%;
-	}
-
-	table, th, td {
-		border: 2px solid black;
-	}
-
-	th, td {
-		padding: 8px;
-		text-align: left;
-	}
-</style>
-
-<table>
-	<thead>
-		<tr>
-			<th>EmployeeID</th>
-			<th>EmpName</th>
-			<th>Salary</th>
-			<th> Image </th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach (var emp in Model)
-		{
-			<tr>
-
-				<td> @emp.EmployeeID</td>
-				<td>@emp.EmpName</td>
-				<td>@emp.Salary</td>
-				<td>
-
-
-					<img src="@emp.ImageUrl"
-						 alt="@emp.EmpName"
-						 width="100" height="100" />
-
-				</td>
-				<td>
-					<a href="/Home/Details/@emp.EmployeeID">Details </a> |
-					<a asp-controller="Home" asp-action="Details" asp-route-id="@emp.EmployeeID">Details </a>
-				</td>
-
-			</tr>
-		}
-
-	</tbody>
-</table>
-
-I am telling here that in home controller there is details method so let us add taht method now 
-
-	public IActionResult Details(int id)
-	{
-		
-
-		var employee = emplist.FirstOrDefault(e => e.EmployeeID == id);
-		if (employee == null)
-		{
-			return NotFound();
-		}
-
-		return View(employee);
-	}
-
-and details view will be like tis 
-------------------------------------
-@model mvcwebapp1.Models.Employee
-@{
-    ViewData["Title"] = "Details";
-}
-
-<h1>Details</h1>
-
-<h1>@Model.EmpName</h1>
-
-<div>
-    <img src="@Model.ImageUrl" alt="@Model.EmpName" width="300" height="300" />
-</div>
-
-<div>
-  
-    <p><strong>Salary:</strong> @Model.Salary</p>
-</div>
-
-<a asp-action="Index">Back to List</a>
-
-Now let us try view model now 
-
-add one department class like this 
-
-namespace MvcFirstExample.Models
-{
-    public class Department
-    {
-
-        public int DeptId { set; get; }
-        public string? DeptName { set; get; }
-    }
-}
-
-
-
- List<Department> deptlist = new List<Department>()
-     {
-         new Department{DeptId=10,DeptName="Sales"},
-         new Department{DeptId=20,DeptName="HR"},
-         new Department{DeptId=30,DeptName="Software"}
-     };
-
-
-public ActionResult viewmodeldemo(int empid)
-{
-	var query1 = deptlist.ToList();
-
-	Employee emp=emplist.Where(x=>x.EmployeeID==empid).FirstOrDefault();
-	var query2 = emp;
-
-	EmpDeptViewModel obj = new EmpDeptViewModel()
-	{
-		deptlist = query1,
-		emp = query2,
-		date = DateTime.Now
-	};
-
-	return View(obj);
-}
-
-Employeeviewmodel view 
--------------------------
-@model MvcFirstExample.Models.EmpDeptViewModel
-@{
-    ViewData["Title"] = "viewmodeldemo";
-}
-
-<h1>viewmodeldemo</h1>
-
-<h2> Date : @Model.date</h2>
-
-<h3>dept details </h3>
-<table border="1" cellpadding="1" cellspacing="1">
-    <tr>
-        <th>DeptID</th>
-        <th>Deptname</th>
-    
-
-    @foreach (Department dept in Model.deptlist)
-    {
-            <tr>
-                <td>@dept.DeptId</td>
-                <td>@dept.DeptName</td>
-            </tr>
-    }
-
-
-</table>
-<h3> Search emp details</h3>
-<table border="1" cellpadding="0" cellspacing="0">
-        <tr>
-            <th>EmployeeID</th>
-            <th>Empname</th>
-            <th>EmpSalary</th>
-        </tr>
-        @if(Model.emp!=null)
-    {
-        <tr>
-            <td>@Model.emp.EmployeeID</td>
-            <td>@Model.emp.EmpName</td>
-            <td>@Model.emp.Salary</td>
-        </tr>
-    }
-    else
-    {
-        <tr>
-            <td>There is no employee with this id</td>
-        </tr>
-    }
-
- </table>
-
-Search Employeee 
-------------------
-public ActionResult searchemp(int empid)
-{
-	Employee emp = (from e1 in emplist where e1.EmployeeID == empid select e1).FirstOrDefault();
-	return View(emp);
-}
-
-search emp view 
-------------------
-@model MvcFirstExample.Models.Employee
-@{
-    ViewData["Title"] = "searchemp";
-}
-
-<h1>searchemp</h1>
-
-  <table border="1" cellpadding="0" cellspacing="0">
-        <tr>
-            <th>EmployeeID</th>
-            <th>Empname</th>
-            <th>EmpSalary</th>
-        </tr>
-        <tr>
-            <td>@Model.EmployeeID</td>
-            <td>@Model.EmpName</td>
-            <td>@Model.Salary</td>
-        </tr>
-    </table>
-
 	
 CRUD operation using a Model :
 -----------------------------------------
@@ -524,6 +193,254 @@ namespace MvcFirstExample.Models
 
     }
 }
+
+Now u have to generate the views graphically but still i am putting generated code view of my programs done earlier 
+
+
+dog create view 
+-------------------
+@model MvcFirstExample.Models.Dog
+
+@{
+    ViewData["Title"] = "Create";
+}
+
+<h1>Create</h1>
+
+<h4>Dog</h4>
+<hr />
+<div class="row">
+    <div class="col-md-4">
+        <form asp-action="Create">
+            <div asp-validation-summary="ModelOnly" class="text-danger"></div>
+            <div class="form-group">
+                <label asp-for="ID" class="control-label"></label>
+                <input asp-for="ID" class="form-control" />
+                <span asp-validation-for="ID" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <label asp-for="Name" class="control-label"></label>
+                <input asp-for="Name" class="form-control" />
+                <span asp-validation-for="Name" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <label asp-for="Age" class="control-label"></label>
+                <input asp-for="Age" class="form-control" />
+                <span asp-validation-for="Age" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Create" class="btn btn-primary" />
+            </div>
+        </form>
+    </div>
+</div>
+
+<div>
+    <a asp-action="Index">Back to List</a>
+</div>
+
+Dog Index view 
+--------------
+@model IEnumerable<MvcFirstExample.Models.Dog>
+
+@{
+    ViewData["Title"] = "Index";
+}
+
+<h1>Index</h1>
+
+<p>
+    <a asp-action="Create">Create New</a>
+</p>
+<table class="table">
+    <thead>
+        <tr>
+            <th>
+                @Html.DisplayNameFor(model => model.ID)
+            </th>
+            <th>
+                @Html.DisplayNameFor(model => model.Name)
+            </th>
+            <th>
+                @Html.DisplayNameFor(model => model.Age)
+            </th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+@foreach (var item in Model) {
+        <tr>
+            <td>
+                @Html.DisplayFor(modelItem => item.ID)
+            </td>
+            <td>
+                @Html.DisplayFor(modelItem => item.Name)
+            </td>
+            <td>
+                @Html.DisplayFor(modelItem => item.Age)
+            </td>
+            <td>
+                @Html.ActionLink("Edit", "Edit", new {  id=item.ID  }) |
+                @Html.ActionLink("Details", "Details", new { id=item.ID }) |
+                @Html.ActionLink("Delete", "Delete", new {  id=item.ID })|
+                @Html.ActionLink("DirectDelete", "DirectDelete", new {  id=item.ID })
+                
+            </td>
+        </tr>
+}
+    </tbody>
+</table>
+
+Dog Edit view 
+---------------
+@model MvcFirstExample.Models.Dog
+
+@{
+    ViewData["Title"] = "Edit";
+}
+
+<h1>Edit</h1>
+
+<h4>Dog</h4>
+<hr />
+<div class="row">
+    <div class="col-md-4">
+        <form asp-action="Edit">
+            <div asp-validation-summary="ModelOnly" class="text-danger"></div>
+            <div class="form-group">
+                <label asp-for="ID" class="control-label"></label>
+                <input asp-for="ID" class="form-control" />
+                <span asp-validation-for="ID" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <label asp-for="Name" class="control-label"></label>
+                <input asp-for="Name" class="form-control" />
+                <span asp-validation-for="Name" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <label asp-for="Age" class="control-label"></label>
+                <input asp-for="Age" class="form-control" />
+                <span asp-validation-for="Age" class="text-danger"></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Save" class="btn btn-primary" />
+            </div>
+        </form>
+    </div>
+</div>
+
+<div>
+    <a asp-action="Index">Back to List</a>
+</div>
+
+Dog Details view 
+---------------
+@model MvcFirstExample.Models.Dog
+
+@{
+    ViewData["Title"] = "Details";
+}
+
+<h1>Details</h1>
+
+<div>
+    <h4>Dog</h4>
+    <hr />
+    <dl class="row">
+        <dt class = "col-sm-2">
+            @Html.DisplayNameFor(model => model.ID)
+        </dt>
+        <dd class = "col-sm-10">
+            @Html.DisplayFor(model => model.ID)
+        </dd>
+        <dt class = "col-sm-2">
+            @Html.DisplayNameFor(model => model.Name)
+        </dt>
+        <dd class = "col-sm-10">
+            @Html.DisplayFor(model => model.Name)
+        </dd>
+        <dt class = "col-sm-2">
+            @Html.DisplayNameFor(model => model.Age)
+        </dt>
+        <dd class = "col-sm-10">
+            @Html.DisplayFor(model => model.Age)
+        </dd>
+    </dl>
+</div>
+<div>
+    @Html.ActionLink("Edit", "Edit", new { /* id = Model.PrimaryKey */ }) |
+    <a asp-action="Index">Back to List</a>
+</div>
+
+Dog delete view 
+------------------
+@model MvcFirstExample.Models.Dog
+
+@{
+    ViewData["Title"] = "Delete";
+}
+
+<h1>Delete</h1>
+
+<h3>Are you sure you want to delete this?</h3>
+<div>
+    <h4>Dog</h4>
+    <hr />
+    <dl class="row">
+        <dt class = "col-sm-2">
+            @Html.DisplayNameFor(model => model.ID)
+        </dt>
+        <dd class = "col-sm-10">
+            @Html.DisplayFor(model => model.ID)
+        </dd>
+        <dt class = "col-sm-2">
+            @Html.DisplayNameFor(model => model.Name)
+        </dt>
+        <dd class = "col-sm-10">
+            @Html.DisplayFor(model => model.Name)
+        </dd>
+        <dt class = "col-sm-2">
+            @Html.DisplayNameFor(model => model.Age)
+        </dt>
+        <dd class = "col-sm-10">
+            @Html.DisplayFor(model => model.Age)
+        </dd>
+    </dl>
+    
+    <form asp-action="Delete">
+        <input type="submit" value="Delete" class="btn btn-danger" /> |
+        <a asp-action="Index">Back to List</a>
+    </form>
+</div>
+
+
+Now i want to execute the same code in BootStrap for better look 
+
+
+Now as i want to make a good bootstrap usage of above code i kept the prompt like this 
+
+prompt for enhancing above dog app
+------------------------------------
+so for the above coding all things i want to implement it using bootswatch.com and i want to use https://bootswatch.com/quartz/
+
+theme here so i want to add one image for the dog using file and also description about the dog so two more properties i want to add into the model class and after adding all dogs i want  list of dogs i want to show in table i want to use quartz tables for displaying 
+
+table data along with small image of 50 by 50 pix display 
+
+In navbar  of quartz i want to provide hyperlinks for create a dog form where i can enter details and image of dog and insert it into 
+
+the tables again and while displaying about dog all details i want to show in 
+
+
+In create form i want file control to browse image from computer and when added it should go to images folder ..
+
+
+I want  complete source code   along with earlier code for each module 
+
+
+now you can refer in mobile the chat gpt generated code for the above prompt and and it is  Dog Management with Image okay and as i had developed the application also so i am keeping that 
+full source code in Day30  folder so you can download that folder in class and by checking prompt response from chat gpt develop the application for the student okay 
+
 
 
 For designing first learn bootstrap from this link 
