706c31a4e84729c9686dc8dca730ad9e67c296e3
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-08-06 12:17:54 +0530

    Update Day40.cs

--- Day40.cs
+++ Day40.cs
@@ -14,10 +14,8 @@ step 2 :
 --------
 after installing this once build the project once
 
-step 3:
-----------
-In Models folder add RegisterUser class like this with annotations 
-
+step 3 :
+---------
 public class RegisterUser
 {
 [Required(ErrorMessage = "User Name is required")]
@@ -27,8 +25,8 @@ public string? Username { get; set; }
 public string? Email { get; set; }
 [Required(ErrorMessage = "Password is required")]
 public string? Password { get; set; }
-}
-}
+} 
+
 
 step 4 :
 --------
@@ -72,6 +70,140 @@ now go to EventDbContext chnage the code like this
         }
     }
     
-here i am adding roles manully though migrations 
+here i am adding roles manully though migrations above here 
+
+Step 4:
+-----------
+Now in program.cs do the following changes 
+
+// For Entity Framework
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(configuration.GetConnectionString("ConnStr")));  //after this u have to add 
+ // For Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+// adding basic authentication
+builder.Services.AddAuthentication(options =>
+{
+options.DefaultAuthenticateScheme =
+JwtBearerDefaults.AuthenticationScheme;
+options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
 
 
+step 5 :
+---------
+---> Now run the migrations commands 
+
+       add-migration InitialCreate
+       update-database
+
+
+--->Now go to dabatabse and analaize the tables once okay these tables will be empty only okay only roles table will be filled 
+
+step 6:
+---------
+add another class Response in Models folder okay ...
+    
+public class Response
+{
+public string? Status { get; set; }
+public string? Message { get; set; }
+}
+
+Next add AuthenticationContrller 
+-----------------------------------
+using IdentityWithTokenDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityWithTokenDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            //Check User Exist 
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", Message = "User already exists!" });
+            }
+
+            //Add the User in the database
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username
+
+            };
+            if (await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "User Failed to Create" });
+                }
+                //Add role to the user....
+
+                await _userManager.AddToRoleAsync(user, role);
+
+
+
+
+
+                return StatusCode(StatusCodes.Status200OK,
+                    new Response { Status = "Success", Message = $"User created SuccessFully" });
+
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "This Role Doesnot Exist." });
+            }
+
+
+        }
+
+       
+
+        
+
+
+    }
+}
+
+step 7 :
+----------
